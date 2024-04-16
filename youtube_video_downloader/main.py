f0b98b4fe007cd2f538b207330eb2181cfaf62d5
yussuf-codes <yussuf.code@gmail.com>
2024-04-15 20:46:15 -0700

    Refactoring

--- youtube_video_downloader/main.py
+++ youtube_video_downloader/main.py
@@ -2,9 +2,9 @@ import subprocess
 
 
 if __name__ == '__main__':
-    YT_DLP_PATH = 'C:\\Users\\Joe\\Documents\\Binaries\\yt-dlp.exe'
+    YT_DLP_PATH_OR_COMMAND = 'C:\\Users\\Joe\\Documents\\Binaries\\yt-dlp.exe'
     DOWNLOAD_DIRECTORY = 'C:\\Users\\Joe\\Videos\\Downloads'
     VIDEO_PATH = DOWNLOAD_DIRECTORY + '/%(title)s.%(ext)s'
-    URL = 'https://www.youtube.com/watch?v=TdrL3QxjyVw'
-    command = [YT_DLP_PATH, '-f', '22', '-o', VIDEO_PATH, URL]
+    URL = 'https://www.youtube.com/watch?v=i_yLpCLMaKk'
+    command = [YT_DLP_PATH_OR_COMMAND, '-f', '22', '-o', VIDEO_PATH, URL]
     subprocess.run(command)
